9ee5841c101977d7e4736316e1f475c26bce8a46
MochJuang <mochjuangpp@gmail.com>
2024-08-13 15:42:38 +0700

    final 3

--- internal/entity/notification.go
+++ internal/entity/notification.go
@@ -8,6 +8,6 @@ type Notification struct {
 	ID        uint      `gorm:"primaryKey"`
 	UserID    uint      `gorm:"not null"`
 	User      User      `gorm:"foreignKey:UserID"`
-	Content   string    `gorm:"type:text;not null"`
+	Message   string    `gorm:"type:text;not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 }
